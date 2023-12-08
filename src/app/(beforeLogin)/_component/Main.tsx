523b9f01e90528583bbdc3a6589a50bd94fa9345
goodchuck <y502100@naver.com>
2023-12-08 15:22:26 +0900

    section 1-5 css module을 선택한 이유

--- src/app/(beforeLogin)/_component/Main.tsx
+++ src/app/(beforeLogin)/_component/Main.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import Link from 'next/link';
-import zLogo from '../../public/zlogo.png';
+import styles from "@/app/(beforeLogin)/_component/main.module.css";
+import Image from "next/image";
+import zLogo from "../../../../public/zlogo.png";
+import Link from "next/link";
 
-export default function Home() {
+export default function Main() {
   return (
-    <div>
+    <>
       <div className={styles.left}>
         <Image src={zLogo} alt="logo" />
       </div>
@@ -14,9 +14,8 @@ export default function Home() {
         <h2>지금 가입하세요.</h2>
         <Link href="/i/flow/signup" className={styles.signup}>계정 만들기</Link>
         <h3>이미 트위터에 가입하셨나요?</h3>
-        {/* login인 이유는 */}
         <Link href="/login" className={styles.login}>로그인</Link>
       </div>
-    </div>
+    </>
   )
 }
--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import {useRouter} from "next/navigation";
+import Main from "@/app/(beforeLogin)/_component/Main";
+
 export default function Login() {
+  const router = useRouter();
+  router.replace('/i/flow/login');
   return (
-    <main>
-      로그인 페이지
-    </main>
+    <Main/>
   );
 }
