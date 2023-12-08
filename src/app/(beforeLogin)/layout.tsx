d6af9f618fd1bc0ec53ac03afc70be304d2e3b81
goodchuck <y502100@naver.com>
2023-12-08 15:37:57 +0900

    1-6 패러렐 라우트

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,10 +1,11 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 import styles from '@/app/(beforeLogin)/_component/main.module.css';
 
 type Props = { children: ReactNode, modal: ReactNode };
 export default function Layout({ children, modal }: Props) {
   return (
     <div className={styles.container}>
+      비포 로그인 레이아웃
       {children}
       {modal}
     </div>
