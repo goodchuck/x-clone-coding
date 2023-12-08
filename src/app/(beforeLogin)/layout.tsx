a669ec5f8f96ab9389bf8b9b452bf82786b7a456
goodchuck <y502100@naver.com>
2023-12-08 16:28:13 +0900

    2-1 강의 내용

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,11 +1,10 @@
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 import styles from '@/app/(beforeLogin)/_component/main.module.css';
 
 type Props = { children: ReactNode, modal: ReactNode };
 export default function Layout({ children, modal }: Props) {
   return (
     <div className={styles.container}>
-      비포 로그인 레이아웃
       {children}
       {modal}
     </div>
